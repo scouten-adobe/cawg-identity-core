b6e177f0cab00c2dfaab9c9d4c6a03519edf8a4d
Eric Scouten <scouten@adobe.com>
2024-08-12 14:14:40 -0700

    Enforce requirement for validFrom field to be populated
    Remove commented-out code to support previous version of ssi crate

--- src/w3c_vc/cose_vc_signature_handler.rs
+++ src/w3c_vc/cose_vc_signature_handler.rs
@@ -142,145 +142,17 @@ impl SignatureHandler for CoseVcSignatureHandler {
             })
             .unwrap();
 
+        // Enforce [§8.1.2.4. Validity].
+        //
+        // [§8.1.2.4. Validity]: https://creator-assertions.github.io/identity/1.x+vc-draft/#vc-property-validFrom
+
+        assert!(asset_vc.valid_from.is_some());
+        // TO DO: Check if ssi crate enforces valid_from < now.
+        // Also check if ssi enforces expiration date.
+
         unimplemented!("Signature verified. Now what?");
 
         /*
-
-        --- REBUILD with SSI 0.8.0
-
-        let mut options = LinkedDataProofOptions::default();
-        options.proof_purpose = Some(ssi_dids::VerificationRelationship::AssertionMethod);
-
-        // TO DO: Support other DID methods.
-        let key_resolver = did_method_key::DIDKey {};
-        let mut loader = cawg_context_loader();
-        let result = vc.verify(Some(options), &key_resolver, &mut loader).await;
-
-        assert!(result.checks.len() == 1);
-        assert!(result.warnings.is_empty());
-        assert!(result.errors.is_empty());
-
-        dbg!(&result);
-
-        // Check VC context requirements.
-
-        // TEMPORARY: Skip this for now because ssi crate isn't ready
-        // for VC V2.
-        // assert!(vc
-        //     .context
-        //     .contains_uri("https://www.w3.org/ns/credentials/v2"));
-
-        assert!(vc.context.contains_uri(CAWG_IDENTITY_CONTEXT_URI));
-
-        // Check VC type requirements.
-
-        assert!(vc.type_.contains(&"VerifiableCredential".to_owned()));
-        assert!(vc.type_.contains(&"CreatorIdentityAssertion".to_owned()));
-
-        */
-
-        /* ---- From older identity prototype ----
-        // NOTE vp is: &Presentation
-
-
-        // Ensure that the proof and VC holder are properly bound.
-        let Some(ref actor_vc) = vp.verifiable_credential else {
-            panic!("HANDLE THIS ERROR: No VC in the VP.")
-        };
-
-        let OneOrMany::One(actor_vc) = actor_vc else {
-            panic!("HANDLE THIS ERROR: VC has multiple credentials.");
-        };
-
-        let CredentialOrJWT::Credential(actor_vc) = actor_vc else {
-            panic!("HANDLE THIS ERROR: VC is actually a JWT.");
-        };
-
-        let OneOrMany::One(ref subject) = actor_vc.credential_subject else {
-            panic!("HANDLE THIS ERROR: Multiple credential subjects.");
-        };
-
-        let Some(ref subject) = subject.id else {
-            panic!("HANDLE THIS ERROR: subject.id missing.");
-        };
-
-        let URI::String(subject) = subject;
-        // ^^ No `else` clause because URI enum has no other
-        // current values.
-
-        // Make sure the presentation correctly identifies the partial claim.
-        let Some(ref proof) = vp.proof else {
-            panic!("HANDLE THIS ERROR: VP doesn't contain a proof.");
-        };
-
-        let OneOrMany::One(proof) = proof else {
-            panic!("HANDLE THIS ERROR: VP contains multiple proofs.");
-        };
-
-        let Some(ref verification_method) = proof.verification_method else {
-            panic!("HANDLE THIS ERROR: VP doens't contain a verification method");
-        };
-
-        // Ignore multibaseValue for this comparison.
-        // TO DO: Is this specific to `did:key`?
-        let verification_method = match verification_method.split_once('#') {
-            Some((vm, _)) => vm,
-            _ => verification_method,
-        };
-
-        let Some(ref domain) = proof.domain else {
-            panic!("HANDLE THIS ERROR: VP doesn't contain a domain");
-        };
-
-        if verification_method != subject {
-            panic!("HANDLE THIS ERROR: Proof doesn't match credential holder");
-        }
-
-        let partial_claim_id = id_for_partial_claim(partial_claim);
-        if domain != &partial_claim_id {
-            panic!("HANDLE THIS ERROR: Proof doesn't match partial claim ID");
-        }
-
-        Ok(())
-        */
-
-        /* ---- From CAWG X.509 branch ----
-        // MAJOR TO DO (POSSIBLE SPEC REVISION): Need to ensure
-        // that the signer_payload we're verifying against is
-        // byte-for-byte identical with what was used for signature.
-        // Current implementation glosses over this.
-
-        let mut signer_payload_cbor: Vec<u8> = vec![];
-        ciborium::into_writer(signer_payload, &mut signer_payload_cbor)
-            .map_err(|_| ValidationError::UnexpectedError)?;
-
-        // -- FROM C2PA CLAIM VALIDATION --
-        // // Parse COSE signed data (signature) and validate it.
-        // let sig = claim.signature_val().clone();
-        // let additional_bytes: Vec<u8> = Vec::new();
-        // let claim_data = claim.data()?;
-
-        let additional_data: Vec<u8> = vec![];
-
-        // TO DO: Allow config of TrustHandler.
-        let mut trust_handler = c2pa::openssl::OpenSSLTrustHandlerConfig::new();
-
-        // TO DO: Allow config of StatusTracker.
-        let mut status_tracker = OneShotStatusTracker::new();
-
-        let verified = verify_cose(
-            signature,
-            &signer_payload_cbor,
-            &additional_data,
-            false,
-            &mut trust_handler,
-            &mut status_tracker,
-        )
-        .unwrap();
-        // TO DO: Error handling.
-
-        // dbg!(&verified);
-
         Ok(Box::new(VcNamedActor(verified)))
         */
     }
