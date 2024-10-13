fb3bbfd81abe81cb7ba1861d07cde02e4973149c
Eric Scouten <scouten@adobe.com>
2024-10-13 15:54:04 -0700

    Refactor Did::new tests

--- src/tests/claim_aggregation/w3c_vc/did.rs
+++ src/tests/claim_aggregation/w3c_vc/did.rs
@@ -18,27 +18,32 @@
 // specific language governing permissions and limitations under
 // each license.
 
-use crate::claim_aggregation::w3c_vc::did::*;
+mod new {
+    use crate::claim_aggregation::w3c_vc::did::Did;
 
-#[test]
-fn parse_did_accept() {
-    let vectors: [&[u8]; 4] = [
-            b"did:method:foo",
-            b"did:a:b",
-            b"did:jwk:eyJjcnYiOiJQLTI1NiIsImt0eSI6IkVDIiwieCI6ImFjYklRaXVNczNpOF91c3pFakoydHBUdFJNNEVVM3l6OTFQSDZDZEgyVjAiLCJ5IjoiX0tjeUxqOXZXTXB0bm1LdG00NkdxRHo4d2Y3NEk1TEtncmwyR3pIM25TRSJ9",
-            b"did:web:example.com%3A443:u:bob"
-        ];
+    #[test]
+    fn valid_dids() {
+        let did = Did::new(b"did:method:foo").unwrap();
+        assert_eq!(did.method_name(), "method");
+        assert_eq!(did.method_specific_id(), "foo");
 
-    for input in vectors {
-        Did::new(input).unwrap();
+        let did = Did::new(b"did:a:b").unwrap();
+        assert_eq!(did.method_name(), "a");
+        assert_eq!(did.method_specific_id(), "b");
+
+        let did = Did::new(b"did:jwk:eyJjcnYiOiJQLTI1NiIsImt0eSI6IkVDIiwieCI6ImFjYklRaXVNczNpOF91c3pFakoydHBUdFJNNEVVM3l6OTFQSDZDZEgyVjAiLCJ5IjoiX0tjeUxqOXZXTXB0bm1LdG00NkdxRHo4d2Y3NEk1TEtncmwyR3pIM25TRSJ9").unwrap();
+        assert_eq!(did.method_name(), "jwk");
+        assert_eq!(did.method_specific_id(), "eyJjcnYiOiJQLTI1NiIsImt0eSI6IkVDIiwieCI6ImFjYklRaXVNczNpOF91c3pFakoydHBUdFJNNEVVM3l6OTFQSDZDZEgyVjAiLCJ5IjoiX0tjeUxqOXZXTXB0bm1LdG00NkdxRHo4d2Y3NEk1TEtncmwyR3pIM25TRSJ9");
+
+        let did = Did::new(b"did:web:example.com%3A443:u:bob").unwrap();
+        assert_eq!(did.method_name(), "web");
+        assert_eq!(did.method_specific_id(), "example.com%3A443:u:bob");
     }
-}
 
-#[test]
-fn parse_did_reject() {
-    let vectors: [&[u8]; 3] = [b"http:a:b", b"did::b", b"did:a:"];
-
-    for input in vectors {
-        assert!(Did::new(input).is_err())
+    #[test]
+    fn err_invalid_did() {
+        Did::new(b"http:a:b").unwrap_err();
+        Did::new(b"did::b").unwrap_err();
+        Did::new(b"did:a:").unwrap_err();
     }
 }
